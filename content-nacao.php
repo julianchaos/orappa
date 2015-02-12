e8df158bcf0883552a31557b5ff43fb5f472949f
Julian Andrade <julianchaos@gmail.com>
2015-02-12 01:42:29 -0200

    - Template para seção Nação O Rappa;  - Imagens do cabeçalho da seção e estilos;

---
+++ content-nacao.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Nação
+ */
+?>
+<section id="page_<?php the_id() ?>" class="content-nacao page_<?php the_id() ?>">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-4 col-xs-offset-4">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/nacao/logo.png"
+					 class="img-responsive"/>
+			</div>
+			<div class="col-xs-12">
+				<h1><?php the_title() ?></h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-10 col-xs-offset-1 excerpt">
+				<?php the_excerpt() ?>
+			</div>
+		</div>
+	</div>
+</section>
