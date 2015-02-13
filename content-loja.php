f2cc67fce03541d4e0163ce823804fb83a7c30d4
Julian Andrade <julianchaos@gmail.com>
2015-02-13 00:06:22 -0200

    - Template da seção Loja;  - Imagens de bg, estilos e fontes da seção Loja

---
+++ content-loja.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Loja
+ */
+?>
+<section id="page_<?php the_id() ?>" class="content-loja page_<?php the_id() ?>">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<h1><?php the_title() ?></h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-12 excerpt">
+				<?php the_content() ?>
+			</div>
+			<div class="col-xs-6 col-xs-offset-3">
+				<a href="<?php the_field('url_loja') ?>" target="_blank" class="button">Acessar</a>
+			</div>
+		</div>
+	</div>
+</section>
