ba3fbd7a1b3067348c988fe9f52aa1183a578a52
Julian Andrade <julianchaos@gmail.com>
2015-02-16 01:27:02 -0200

    - Imagem que substitui o carrossel em telas pequenas (xs);  - Carrossel em todos os tamanhos que usam ele (sm, md e lg);

--- content-carousel.php
+++ content-carousel.php
@@ -1,37 +1,54 @@
 <section id="content_carousel" class="content-carousel">
-	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<?php
+if( have_rows('carrossel')) :
+	$totalImages = count(get_field('carrossel'));
+?>
+	<div id="carousel-generic" class="carousel slide hidden-xs" data-ride="carousel">
 		<!-- Indicators -->
 		<ol class="carousel-indicators">
-			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-			<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+<?php
+	for ($i = 0; $i < $totalImages; $i++) {
+		echo "<li data-target='#carousel-generic' data-slide-to='$i' ";
+		if($i===0) {echo "class='active'";}
+		echo "></li>";
+	}
+?>
+			
 		</ol>
 
 		<!-- Wrapper for slides -->
 		<div class="carousel-inner" role="listbox">
-			<div class="item active">
-				<img src="..." alt="...">
-				<div class="carousel-caption">
-					...
+<?php
+	$i = 0;
+	while( have_rows('carrossel')) :
+		the_row(); 
+		$carrosselImgObj = get_sub_field('imagem_carrossel'); ?>
+			<div class="item <?php if($i===0){echo 'active';}?>">
+				<div class="img-bg">
+					<img src="<?php echo $carrosselImgObj['url'] ?>" width="<?php echo $carrosselImgObj['width'] ?>" height="<?php echo $carrosselImgObj['height'] ?>">
 				</div>
+				<img src="<?php echo $carrosselImgObj['url'] ?>" width="<?php echo $carrosselImgObj['width'] ?>" height="<?php echo $carrosselImgObj['height'] ?>">
 			</div>
-			<div class="item">
-				<img src="..." alt="...">
-				<div class="carousel-caption">
-					...
-				</div>
-			</div>
-			...
+			
+<?php
+		$i++;
+	endwhile; ?>
 		</div>
 
 		<!-- Controls -->
-		<a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		<a class="left carousel-control" href="#carousel-generic" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		<a class="right carousel-control" href="#carousel-generic" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
+	</div>
+<?php
+endif;
+?>
+	<div class="visible-xs-block">
+		<img src="<?php the_field('imagem_telas_pequenas') ?>" class="carousel-xs-img" />
 	</div>
 </section>
--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,41 @@
 jQuery(function () {
+	//Home Carrossel
+	var carouselHeight = function(){ return jQuery(window).height() - jQuery('header').height()}
+	function resizeCarrossel()
+	{
+		jQuery('#carousel-generic').height(carouselHeight());
+		jQuery('#carousel-generic .item').height(carouselHeight());
+		
+		var carouselSizeProportion = jQuery(window).width() / carouselHeight();
+		jQuery('#carousel-generic .item > img').each(function(){
+			jQuery(this).css('marginTop', 0);
+			
+			var imageSizeProportion = jQuery(this).attr('width') / jQuery(this).attr('height');
+			if(carouselSizeProportion < imageSizeProportion) {
+				var imgNewWidth = jQuery(window).width();
+				var imgNewHeight = jQuery(window).width()/imageSizeProportion;
+				
+				jQuery(this).width(imgNewWidth);
+				jQuery(this).height(imgNewHeight);
+				jQuery(this).css('marginTop',  ( carouselHeight()-imgNewHeight ) / 2 );
+			}
+			else
+			{
+				var imgNewWidth = carouselHeight() * imageSizeProportion;
+				var imgNewHeight = carouselHeight();
+				
+				jQuery(this).width(imgNewWidth);
+				jQuery(this).height(imgNewHeight);
+			}
+		});
+		jQuery('#carousel-generic .item .img-bg img').each(function(){
+			jQuery(this).height(carouselHeight()*2);
+			jQuery(this).width(jQuery(window).width()*2);
+		});
+	}
+	resizeCarrossel();
+	jQuery(window).resize(resizeCarrossel);
+	
 	//Agenda
 	jQuery('[data-toggle="tooltip"]').tooltip();
 	
