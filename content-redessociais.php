bbdd83af04b52aa6e6f1ca75005baf5c4527026d
Julian Andrade <julianchaos@gmail.com>
2015-02-25 01:12:13 -0300

    Adição dos embeds das redes sociais

--- content-redessociais.php
+++ content-redessociais.php
@@ -13,7 +13,7 @@
 				<div class="rede-social-item">
 					<h4>Soundcloud</h4>
 					<div>
-						Flickr player
+						<iframe width="100%" height="300" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/59484115&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
 					</div>
 				</div>
 			</div>
@@ -30,7 +30,7 @@
 					<div class="col-xs-12 col-lg-6 rede-social-item">
 						<h4>Facebook</h4>
 						<div>
-							facebook feed
+							<iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Forappa&amp;width&amp;height=395&amp;colorscheme=dark&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=false&amp;appId=525999617415472" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:395px;" allowTransparency="true"></iframe>
 						</div>
 					</div>
 				</div>
@@ -39,7 +39,7 @@
 				<div class="rede-social-item">
 					<h4>Flickr</h4>
 					<div>
-						Flickr feed
+						<div style='position: relative; padding-bottom: 101%; height: 0; overflow: hidden;'><iframe id='iframe' src='http://flickrit.com/slideshowholder.php?height=100&size=big&userId=orappa_oficial&thumbnails=0&transition=0&layoutType=responsive&sort=0' scrolling='no' frameborder='0'style='width:100%; height:100%; position: absolute; top:0; left:0;' ></iframe></div>
 					</div>
 				</div>
 			</div>
@@ -47,7 +47,7 @@
 				<div class="rede-social-item">
 					<h4>Instagram</h4>
 					<div>
-						Instagram Feed
+						<?php echo do_shortcode('[enjoyinstagram_mb_grid]') ?>
 					</div>
 				</div>
 			</div>
