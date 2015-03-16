8221151428c56c0ef40cde7fb6d89aab4c3841bc
Julian Andrade <julianchaos@gmail.com>
2015-03-15 21:07:08 -0300

    Correção dos tamanhos dos iFrames de redes sociais. A montagem do iframe do twitter estava extrapolando o tamanho do seu container. As modificações funcionaram bem no iPad 1

--- header.php
+++ header.php
@@ -8,9 +8,17 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 	<head profile="http://gmpg.org/xfn/11">
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+			
 		<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
-		<meta name="viewport" content="width=device-width" />
+		
+		<!--<meta name="viewport" content="width=device-width" />-->
+		
+		<!--<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1,width=device-width">-->
+		<!--<meta name="viewport" content="initial-scale=1,user-scalable=no,maximum-scale=1">-->
 
 		<?php wp_head() ?>
 		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
